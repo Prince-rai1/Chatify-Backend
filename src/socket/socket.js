467275b305be8239acdca38aa0eaf3e1c8f323ad
Prince Rai <princeraia578@gmail.com>
2026-07-15 19:40:35 +0530

    Update CORS configuration to use environment variable for client URI

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -9,7 +9,7 @@ const userSocketMap = new Map();
 const initializeSocket = (server) => {
   io = new Server(server, {
     cors: {
-      origin: "http://localhost:5173",
+      origin:process.env.CLIENT_URI,
       credentials: true,
     },
   });
